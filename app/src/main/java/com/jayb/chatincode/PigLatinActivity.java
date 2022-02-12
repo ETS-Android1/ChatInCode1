285cdb1c9e5307300351aecc2de80f398c6f0348
Jamie Braaksma <j.braaksma331@mybvc.ca>
2022-02-12 15:21:34 -0700

    Finished initial Pig Latin UI. Finished initial Create Account UI. Fixed Manifest merger error by adding exported = true to all xml layout files and AndroidManifest.xml

---
+++ app/src/main/java/com/jayb/chatincode/PigLatinActivity.java
@@ -0,0 +1,14 @@
+package com.jayb.chatincode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PigLatinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pig_latin);
+    }
+}
