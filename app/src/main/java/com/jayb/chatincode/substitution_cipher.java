878b124f00df6b6bf4e5dc950dd6f525eedcae79
Jamie Braaksma <j.braaksma331@mybvc.ca>
2022-02-14 14:13:33 -0700

    Finish initial caesar sub-cipher UI.

---
+++ app/src/main/java/com/jayb/chatincode/substitution_cipher.java
@@ -0,0 +1,14 @@
+package com.jayb.chatincode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class substitution_cipher extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_substitution_cipher);
+    }
+}
