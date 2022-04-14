21ee4a96e42f2effe0d4655a785daa2654c6fe26
Jamie Braaksma <j.braaksma331@mybvc.ca>
2022-04-14 02:18:53 -0600

    Add Firebase authentication starter code. Needs to be finished.

--- app/src/main/java/com/jayb/chatincode/CreateAccountActivity.java
+++ app/src/main/java/com/jayb/chatincode/CreateAccountActivity.java
@@ -1,14 +1,71 @@
 package com.jayb.chatincode;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class CreateAccountActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private String TAG = "CREATE_ACCOUNT_ACTIVITY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    //Firebase documentation provided authentication code outline to follow https://firebase.google.com/docs/auth/android/password-auth?authuser=0&hl=en
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent homeIntent = new Intent(CreateAccountActivity.this, HomePageActivity.class);
+            startActivity(homeIntent);
+        }
+    }
+
+    private void createNewUser(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            userSignedIn(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(CreateAccountActivity.this, "Authentication failed.", Toast.LENGTH_LONG).show();
+
+                            userSignedIn(null);
+                        }
+                    }
+                });
+    }
+
+    private void userSignedIn(FirebaseUser user) {
+        if(user == null) {
+            //TODO display message to user to attempt again
+        }
+        else {
+            //TODO Handle successful sign-up
+
+        }
     }
 }
--- app/src/main/java/com/jayb/chatincode/MainActivity.java
+++ app/src/main/java/com/jayb/chatincode/MainActivity.java
@@ -1,14 +1,69 @@
 package com.jayb.chatincode;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private String TAG = "MAIN_ACTIVITY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    //Firebase documentation provided authentication code outline to follow https://firebase.google.com/docs/auth/android/password-auth?authuser=0&hl=en
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null) {
+            Intent homeIntent = new Intent(MainActivity.this, HomePageActivity.class);
+            startActivity(homeIntent);
+        }
+    }
+
+    private void signInUser (String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            userLoggedIn(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                            userLoggedIn(null);
+                        }
+                    }
+                });
+    }
+
+    private void userLoggedIn(FirebaseUser user) {
+        if(user == null) {
+            //TODO display message to user to attempt again
+        }
+        else {
+            //TODO Handle successful sign-up
+
+        }
     }
 }
