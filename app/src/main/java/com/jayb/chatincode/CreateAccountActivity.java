b006be43be9b6a66cf038a80d79919a1e52c0a4c
Jamie Braaksma <j.braaksma331@mybvc.ca>
2022-04-14 03:27:33 -0600

    Finished and tested create an account and sign-in and sign-out on app close.

--- app/src/main/java/com/jayb/chatincode/CreateAccountActivity.java
+++ app/src/main/java/com/jayb/chatincode/CreateAccountActivity.java
@@ -1,42 +1,45 @@
 package com.jayb.chatincode;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener{
 
     private FirebaseAuth mAuth;
-    private String TAG = "CREATE_ACCOUNT_ACTIVITY";
+    private final String TAG = "CREATE_ACCOUNT_ACTIVITY";
     Button createAccBtn;
     EditText nameInput;
     EditText passInput;
     EditText passConfInput;
+    TextView haveAccTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
         mAuth = FirebaseAuth.getInstance();
+
         nameInput = findViewById(R.id.createNameInput);
         passInput = findViewById(R.id.createPassInput);
         passConfInput = findViewById(R.id.confirmPassInput);
+        haveAccTxt = findViewById(R.id.haveAccTxt);
         createAccBtn = findViewById(R.id.createAccBtn);
+
         createAccBtn.setOnClickListener(this);
+        haveAccTxt.setOnClickListener(this);
     }
+
 
     @Override
     public void onClick(View view) {
@@ -50,7 +53,7 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
                 createNewUser(username, pass);
             }
         }
-        else if (true) {
+        else if (id == R.id.haveAccTxt) {
             Intent signInIntent = new Intent(CreateAccountActivity.this, MainActivity.class);
             startActivity(signInIntent);
         }
@@ -59,10 +62,12 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
     private boolean validateUserInput(String username, String pass, String passConf) {
 
         if(username.isEmpty() || pass.isEmpty() || passConf.isEmpty()) {
+            Log.e(TAG, "Create account attempt with missing information. Create account aborted.");
             Toast.makeText(this, "Please fill out all fields.", Toast.LENGTH_LONG).show();
             return false;
         }
         else if (!pass.equals(passConf)) {
+            Log.e(TAG, "Create account attempt with mismatch passwords. Create account aborted.");
             Toast.makeText(this, "Passwords do not match.", Toast.LENGTH_LONG).show();
             return false;
         }
@@ -84,28 +89,24 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if(currentUser != null){
-            Intent homeIntent = new Intent(CreateAccountActivity.this, HomePageActivity.class);
-            startActivity(homeIntent);
+            finish();
         }
     }
 
     private void createNewUser(String username, String password) {
         mAuth.createUserWithEmailAndPassword(username, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success. User: " + username);
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            userSignedIn(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.e(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(CreateAccountActivity.this, "Authentication failed.", Toast.LENGTH_LONG).show();
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success. User: " + username);
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        userSignedIn(user);
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.e(TAG, "createUserWithEmail:failure", task.getException());
+                        Toast.makeText(CreateAccountActivity.this, "Authentication failed.", Toast.LENGTH_LONG).show();
 
-                            userSignedIn(null);
-                        }
+                        userSignedIn(null);
                     }
                 });
     }
--- app/src/main/java/com/jayb/chatincode/MainActivity.java
+++ app/src/main/java/com/jayb/chatincode/MainActivity.java
@@ -1,29 +1,68 @@
 package com.jayb.chatincode;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private FirebaseAuth mAuth;
-    private String TAG = "MAIN_ACTIVITY";
+    private final String TAG = "MAIN_ACTIVITY";
+    private EditText nameInput, passInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mAuth = FirebaseAuth.getInstance();
+        nameInput = findViewById(R.id.loginNameInput);
+        passInput = findViewById(R.id.loginPassInput);
+        TextView needAccText = findViewById(R.id.noAccTxt);
+        Button signInBtn = findViewById(R.id.loginBtn);
+
+        needAccText.setOnClickListener(this);
+        signInBtn.setOnClickListener(this);
+
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        FirebaseAuth.getInstance().signOut();
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        if(id == R.id.loginBtn) {
+            String username = nameInput.getText().toString();
+            String password = passInput.getText().toString();
+            if(username.isEmpty() || password.isEmpty()) {
+                Log.e(TAG, "Sign-in attempt with missing information. Sign-in aborted.");
+                Toast.makeText(this, "Please fill out all fields", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                signInUser(username, password);
+            }
+        }
+        else if (id == R.id.noAccTxt) {
+            Intent createAccIntent = new Intent(MainActivity.this, CreateAccountActivity.class);
+            startActivity(createAccIntent);
+        }
+    }
+
 
     //Firebase documentation provided authentication code outline to follow https://firebase.google.com/docs/auth/android/password-auth?authuser=0&hl=en
     @Override
@@ -39,31 +78,29 @@ public class MainActivity extends AppCompatActivity {
 
     private void signInUser (String email, String password) {
         mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            userLoggedIn(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithEmail:failure", task.getException());
-                            Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
-                            userLoggedIn(null);
-                        }
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        userLoggedIn(user);
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.getException());
+                        Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        userLoggedIn(null);
                     }
                 });
     }
 
     private void userLoggedIn(FirebaseUser user) {
         if(user == null) {
-            //TODO display message to user to attempt again
+            Toast.makeText(this, "Error logging in. Please try again.", Toast.LENGTH_SHORT).show();
         }
         else {
-            //TODO Handle successful sign-up
-
+            Intent homeIntent = new Intent(MainActivity.this, HomePageActivity.class);
+            startActivity(homeIntent);
         }
     }
+
 }
