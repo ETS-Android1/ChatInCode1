192fab29d940a62b45d983be0b18e849c87524a7
Jamie Braaksma <j.braaksma331@mybvc.ca>
2022-04-17 01:08:12 -0600

    Add saved_cipher activity

---
+++ app/src/main/java/com/jayb/chatincode/Saved_Ciphers.java
@@ -0,0 +1,14 @@
+package com.jayb.chatincode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Saved_Ciphers extends AppCompatActivity {
+    private final String TAG = "SAVED_CIPHERS";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved_ciphers);
+    }
+}
