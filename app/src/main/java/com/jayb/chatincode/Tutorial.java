d3081ab9e3ff2185bcd60c4ae3ecf651c2de7d13
Jamie Braaksma <j.braaksma331@mybvc.ca>
2022-04-22 17:14:09 -0600

    Update alt layouts for new instruct button

--- app/src/main/java/com/jayb/chatincode/Tutorial.java
+++ app/src/main/java/com/jayb/chatincode/Tutorial.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
+import java.util.Objects;
+
 public class Tutorial extends AppCompatActivity {
     private final String TAG = "TUTORIAL";
     Button homeBtn;
@@ -12,6 +14,7 @@ public class Tutorial extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial);
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Tutorial");
 
         homeBtn = findViewById(R.id.homeBtn);
         homeBtn.setOnClickListener(v -> finish());
